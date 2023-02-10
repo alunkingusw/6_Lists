caa913c9d9630dea1b451474db55da3123c1c356
Alun <alun.king1@southwales.ac.uk>
2023-02-10 11:16:19 +0000

    Initial Commit

---
+++ ListsDemoExpandable/ListsDemoExpandableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsDemoExpandableApp.swift
+//  ListsDemoExpandable
+//
+//  Created by Alun King on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListsDemoExpandableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
